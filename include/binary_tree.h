f727ea3a51ddaf2c271f11c5037d272eb620cea9
Jasmine T <tanghocle456@gmail.com>
2023-10-05 02:41:11 -0700

    Fix: Fix compile error on CI/CD

--- include/binary_tree.h
+++ include/binary_tree.h
@@ -3,7 +3,9 @@
 //
 #ifndef CPP_TREES_BINARY_TREE_H
 #define CPP_TREES_BINARY_TREE_H
+
 #include <optional>
+#include <memory>
 
 template<typename K, typename V>
 class binary_tree {
@@ -25,8 +27,8 @@ int64_t binary_tree<K, V>::size() {
     int64_t tempSize {};
 
     if (this->key.has_value()) tempSize += 1;
-    if (left_tree != nullptr) tempSize += left_tree->size();
-    if (right_tree != nullptr) tempSize += right_tree->size();
+    if (this->left_tree != nullptr) tempSize += this->left_tree->size();
+    if (this->right_tree != nullptr) tempSize += this->right_tree->size();
 
     return tempSize;
 }
@@ -35,8 +37,8 @@ template<typename K, typename V>
 binary_tree<K, V>::binary_tree() {
     this->key = std::nullopt;
     this->value = std::nullopt;
-    left_tree = nullptr;
-    right_tree = nullptr;
+    this->left_tree = nullptr;
+    this->right_tree = nullptr;
 }
 
 template<typename K, typename V>
@@ -64,11 +66,11 @@ bool binary_tree<K, V>::contains(K key) {
     } else if (this->key.value() == key) {
         return true;
     } else if (this->key.value() < key) {
-        if (left_tree == nullptr) return false;
-        return left_tree->contains(key);
+        if (this->left_tree == nullptr) return false;
+        return this->left_tree->contains(key);
     } else {
-        if (right_tree == nullptr) return false;
-        return right_tree->contains(key);
+        if (this->right_tree == nullptr) return false;
+        return this->right_tree->contains(key);
     }
 }
 
@@ -80,11 +82,11 @@ std::optional<V> binary_tree<K, V>::find(K key) {
         return this->value.value();
     }
     else if (this->key.value() < key) {
-        if (left_tree == nullptr) return -1;
-        return left_tree->find(key);
+        if (this->left_tree == nullptr) return -1;
+        return this->left_tree->find(key);
     } else {
-        if (right_tree == nullptr) return -1;
-        return right_tree->find(key);
+        if (this->right_tree == nullptr) return -1;
+        return this->right_tree->find(key);
     }
 }
 
