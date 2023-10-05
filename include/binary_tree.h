7f5a89c036beefb8cf7fa9e4f9b0352ed11d1a48
Jasmine T <tanghocle456@gmail.com>
2023-10-05 02:03:44 -0700

    Feature: Add templating to binary_tree

--- include/binary_tree.h
+++ include/binary_tree.h
@@ -3,19 +3,76 @@
 //
 #ifndef CPP_TREES_BINARY_TREE_H
 #define CPP_TREES_BINARY_TREE_H
+#include <optional>
 
 template<typename K, typename V>
 class binary_tree {
 private:
-    K key;
-    V value;
+    std::optional<K> key;
+    std::optional<V> value;
     binary_tree<K, V> *left_tree, *right_tree;
 public:
     binary_tree<K, V>();
-    void insert(int key, int value);
-    bool contains(int key);
-    int find(int key);
+    void insert(K key, V value);
+    bool contains(K key);
+    std::optional<V> find(K key);
 };
 
+template<typename K, typename V>
+binary_tree<K, V>::binary_tree() {
+    this->key = std::nullopt;
+    this->value = std::nullopt;
+    left_tree = nullptr;
+    right_tree = nullptr;
+}
+
+template<typename K, typename V>
+void binary_tree<K, V>::insert(K key, V value) {
+    if (this->key == std::nullopt || this->key.value() == key) {
+        this->key = std::make_optional(key);
+        this->value = value;
+    } else if (this->key.value() < key) {
+        if (this->left_tree == nullptr) {
+            this->left_tree = new binary_tree;
+        }
+        this->left_tree->insert(key, value);
+    } else {
+        if (this->right_tree == nullptr) {
+            this->right_tree = new binary_tree;
+        }
+        this->right_tree->insert(key, value);
+    }
+}
+
+template<typename K, typename V>
+bool binary_tree<K, V>::contains(K key) {
+    if (this->key == std::nullopt) {
+        return false;
+    } else if (this->key.value() == key) {
+        return true;
+    } else if (this->key.value() < key) {
+        if (left_tree == nullptr) return false;
+        return left_tree->contains(key);
+    } else {
+        if (right_tree == nullptr) return false;
+        return right_tree->contains(key);
+    }
+}
+
+template<typename K, typename V>
+std::optional<V> binary_tree<K, V>::find(K key) {
+    if (this->key == std::nullopt) {
+        return std::nullopt;
+    } else if (this->key.value() == key) {
+        return this->value.value();
+    }
+    else if (this->key.value() < key) {
+        if (left_tree == nullptr) return -1;
+        return left_tree->find(key);
+    } else {
+        if (right_tree == nullptr) return -1;
+        return right_tree->find(key);
+    }
+}
 
 #endif //CPP_TREES_BINARY_TREE_H
--- src/binary_tree/tests/test_insert.cpp
+++ src/binary_tree/tests/test_insert.cpp
@@ -4,8 +4,29 @@
 
 #include "gtest/gtest.h"
 #include "binary_tree.h"
+
 TEST(INSERTION, basic_insert) {
-    binary_tree basic_tree = binary_tree();
+    binary_tree basic_tree = binary_tree<int, int>();
     basic_tree.insert(1 ,1);
     ASSERT_EQ(basic_tree.contains(1), true);
+    ASSERT_EQ(basic_tree.find(1), 1);
+}
+
+TEST(INSERTION, automatic_identity_insert) {
+    binary_tree basic_tree = binary_tree<int, int>();
+    for (int i = 0; i < 1000; i++) {
+        basic_tree.insert(i, i);
+        ASSERT_EQ(basic_tree.contains(i), true);
+        ASSERT_EQ(basic_tree.find(i).value(), i);
+    }
+}
+
+TEST(INSERTION, automatic_randomized_insert) {
+    binary_tree basic_tree = binary_tree<int, int>();
+    for (int i = 0; i < 100000; i++) {
+        int rand = std::rand();
+        basic_tree.insert(rand, i);
+        ASSERT_EQ(basic_tree.contains(rand), true);
+        ASSERT_EQ(basic_tree.find(rand).value(), i);
+    }
 }
